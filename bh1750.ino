760a7534f7f7773eac0b81fc870f391a684f19c2
husxk <mpacholczyk31s@gmail.com>
2023-02-18 13:29:39 +0100

    change of displayInfo() and data types

--- bh1750.ino
+++ bh1750.ino
@@ -5,10 +5,10 @@
 BH1750 lightMeter;
 
 float wattsGenerated = 0; // w/m2
-int examineTime = 0; // s
+long examineTime = 0; // s
 
 float wattsGeneratedReaded;
-int examineTimeReaded;
+long examineTimeReaded;
 
 void readDataFromEEPROM(int);
 
@@ -17,13 +17,13 @@ void setup()
   Serial.begin(9600);         
   Wire.begin();               // Enable I2C pins of Arduino
   lightMeter.begin();
-  EEPROM.begin(sizeof(float)+ sizeof(int));
+  EEPROM.begin(sizeof(float)+ sizeof(long));
   delay(2000);
 
   readDataFromEEPROM(0);
   Serial.println();
-  Serial.print(wattsGeneratedReaded);
-  Serial.print("kW overall, examineded in time (s): ");
+  Serial.print(wattsGeneratedReaded * 3600);
+  Serial.print("kWh/m2 overall, examineded in time (s): ");
   Serial.println(examineTimeReaded);
   delay(5000);
 }
@@ -31,13 +31,17 @@ void setup()
 void displayInfo(float lux, float watts, float wattsGenerated, float toNow)
 {
   Serial.print(lux);                       
-  Serial.print("lx, watss now: "); 
+  Serial.print("lx now, watss now: "); 
   Serial.print(watts);
   Serial.print("W, overall watts generated: ");
   Serial.print(wattsGenerated);
-  Serial.print("kW, to now: ");
-  Serial.print(toNow);
-  Serial.println("W ");
+  Serial.print("kW, avarage to now: ");
+  Serial.print(toNow * 3600);
+  Serial.print("kWh/m2/time, in time ");
+  Serial.print(examineTime);
+  Serial.print("s, in hours: ");
+  Serial.print(float(examineTime / 3600));
+  Serial.println("h");
 }
 
 bool writeDataToEEPROM(int address, int data)
@@ -52,11 +56,11 @@ bool writeDataToEEPROM(int address, int data)
 
 void isSuccess(bool success)
 {
-  Serial.print("Saving to EEPROM... Return:  ");
+  Serial.print("Saving to EEPROM... Return: ");
   Serial.println(success);
 }
 
-void saveData(int address) // 0
+void saveData(int address) // default: 0
 {
   bool success = writeDataToEEPROM(address, 1);
   isSuccess(success);
@@ -65,7 +69,7 @@ void saveData(int address) // 0
   isSuccess(success);
 }
 
-void readDataFromEEPROM(int address) // 0
+void readDataFromEEPROM(int address) // deafault: 0
 {
   wattsGeneratedReaded = EEPROM.read(address);
   examineTimeReaded = EEPROM.read(address + sizeof(float));
@@ -75,9 +79,9 @@ void loop()
 {
   float lux = lightMeter.readLightLevel();
   float watts = lux * 0.0079;
-  wattsGenerated += watts / 1000; //kW
+  wattsGenerated += watts / 1000; // kW
   examineTime++;
-  float toNow = wattsGenerated/examineTime; /* ~0.72W/m2 dziennie okolo 2,612 kWh/m2/dzien 
+  float toNow = wattsGenerated / examineTime; /* ~0.72W/m2 dziennie okolo 2,612 kWh/m2/dzien 
                                               luty srednio 0.99kwh/m2/day */
 
   displayInfo(lux, watts, wattsGenerated, toNow);
