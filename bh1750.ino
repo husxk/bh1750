319ddfb81ffb719b0425b9088875d1f4d6b47a95
husxk <mpacholczyk31s@gmail.com>
2023-02-20 12:35:23 +0100

    change of formula of counting Wh/m2
    change formula of insolation measurement, change counting of Wh/m2, change displayInfo()

--- bh1750.ino
+++ bh1750.ino
@@ -5,6 +5,8 @@ BH1750 lightMeter;
 
 float wattsGenerated = 0; // w/m2
 long examineTime = 0; // s
+float insolationInDay = 0;
+float overallInsolationInDay = 0;
 
 void setup()
 {
@@ -14,7 +16,7 @@ void setup()
   delay(2000);
 }
 
-void displayInfo(float lux, float watts, float wattsGenerated, float isolationThisDay)
+void displayInfo(float lux, float watts, float wattsGenerated, float insolationThisDay, float insolationNow)
 {
   Serial.print(lux);                       
   Serial.print("lx now, watts now: "); 
@@ -24,20 +26,31 @@ void displayInfo(float lux, float watts, float wattsGenerated, float isolationTh
   Serial.print("kW, in time ");
   Serial.print(examineTime);
   Serial.print("s, overall: ");
-  Serial.print(isolationThisDay);
-  Serial.println("kWh/m2/day");
+  Serial.print(insolationThisDay);
+  Serial.print("kWh/m2/day, another measurement: insolation now:");
+  Serial.print(insolationNow);
+  Serial.print("Wh/m2/now, ");
+  Serial.print(insolationInDay);
+  Serial.print("Wh/m2/day, overall insolation: ");
+  Serial.print(overallInsolationInDay);
+  Serial.println("Wh/m2");
 }
 
 void loop()
 {
   float lux = lightMeter.readLightLevel();
-  float watts = lux * 0.0079;
+  float watts = lux * 0.0079; // w/m2
+  
   wattsGenerated += (watts / 1000); // kW
-  examineTime++; /* ~0.72W/m2 dziennie okolo 2,612 kWh/m2/dzien 
-                                              luty srednio 0.99kwh/m2/day */
-  float isolationThisDay = wattsGenerated / 3600; // wattsGenerated * 24 [kWh] / ( 24[hours examinated] * 3600[every examine] ) [kWh/m2/day]
+  examineTime++;                            /* ~0.72W/m2 dziennie okolo 2,612 kWh/m2/dzien // luty srednio 0.99kwh/m2/day */
 
-  displayInfo(lux, watts, wattsGenerated, isolationThisDay);
+  float insolationThisDay = wattsGenerated / 3600; // wattsGenerated * 24 [kWh] / ( 24[hours examinated] * 3600[every examine] ) [kWh/m2/day]
+
+  float insolationNow = (watts / 3600); // wh/m2
+  overallInsolationInDay += insolationNow; 
+  insolationInDay = (insolationInDay / 24) / 3600;  
+
+  displayInfo(lux, watts, wattsGenerated, insolationThisDay, insolationNow);
   
   delay(1000);
 }
